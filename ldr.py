6174b67f2497ff43b364583e0f1b36d180a76fdb
yaridb <43003390+Yarideboed@users.noreply.github.com>
2020-04-05 13:02:11 +0200

    goede code geüpload van ldr.py

--- ldr.py
+++ ldr.py
@@ -1,20 +1,37 @@
-#!/usr/bin/python
- 
-import spidev
+import RPi.GPIO as GPIO
 import time
- 
-spi = spidev.SpiDev()
-spi.open(0,0)
- 
-def read_spi(channel):
-  spidata = spi.xfer2([1,(8+channel)<<4,0])
-  return ((spidata[1] & 3) << 8) + spidata[2]
- 
+GPIO.setmode(GPIO.BOARD)
+delayt = .1 
+value = 0 # this variable will be used to store the ldr value
+ldr = 7 #ldr is connected with pin number 7
+
+def rc_time (ldr):
+    count = 0
+    #Output on the pin for
+    GPIO.setup(ldr, GPIO.OUT)
+    GPIO.output(ldr, False)
+    time.sleep(delayt)
+
+    #Change the pin back to input
+    GPIO.setup(ldr, GPIO.IN)
+
+    #Count until the pin goes high
+    while (GPIO.input(ldr) == 0):
+        count += 1
+
+    return count
+#Catch when script is interrupted, cleanup correctly
 try:
-  while True:
-    channeldata = read_spi(0)
-    print("Waarde = {}".format(channeldata))
-    time.sleep(.1)
- 
+    # Main loop
+    while True:
+        print("Ldr Value:")
+        value = rc_time(ldr)
+        print(value)
+        if ( value <= 10000 ):
+                print("Lights are ON")
+        if (value > 10000):
+                print("Lights are OFF")
 except KeyboardInterrupt:
-  spi.close()
+    pass
+finally:
+    GPIO.cleanup()
